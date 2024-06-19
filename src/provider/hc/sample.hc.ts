5bf5643669c39cf32aaeb02cb4d8ce5896ff4511
Jay Lim <mycheesepasta@gmail.com>
2024-06-19 19:41:18 +0900

    fix(provider/hc): comment all features since this is an example

--- src/provider/hc/sample.hc.ts
+++ src/provider/hc/sample.hc.ts
@@ -1,22 +1,27 @@
-import { Observable, Subject } from 'rxjs';
-import { SystemStatus } from '../../constants/SystemStatus';
+//* HC module for implementing providers.
 
-class SampleHc {
-    private _subject = new Subject<SystemStatus>();
+//* The server health checker will subscribe these modules to check provider's status (db, mq, etc...)
+//* Define hc for each providers based on the sample provided below.
 
-    public get observable(): Observable<SystemStatus> {
-        return this._subject.asObservable();
-    }
-
-    public isReady() {
-        this._subject.next(SystemStatus.BOOTSTRAPPED);
-    }
-
-    public isDead() {
-        this._subject.next(SystemStatus.FLATLINE);
-    }
-}
-
-const sampleHc = new SampleHc();
-const sampleHcObservable = sampleHc.observable;
-export default sampleHcObservable;
+// import { Observable, Subject } from 'rxjs';
+// import { SystemStatus } from '../../constants/SystemStatus';
+//
+// class SampleHc {
+//     private _subject = new Subject<SystemStatus>();
+//
+//     public get observable(): Observable<SystemStatus> {
+//         return this._subject.asObservable();
+//     }
+//
+//     public isReady() {
+//         this._subject.next(SystemStatus.BOOTSTRAPPED);
+//     }
+//
+//     public isDead() {
+//         this._subject.next(SystemStatus.FLATLINE);
+//     }
+// }
+//
+// const sampleHc = new SampleHc();
+// const sampleHcObservable = sampleHc.observable;
+// export default sampleHcObservable;
